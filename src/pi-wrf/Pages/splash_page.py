cbf29cf66c55bdf438973e0eca2380dfa316bdca
wefoust <wefoust@ucar.edu>
2020-09-08 12:54:55 -0400

    refactor to improve consistency and readability

--- src/pi-wrf/Pages/splash_page.py
+++ src/pi-wrf/Pages/splash_page.py
@@ -1,13 +1,12 @@
-#Importing standard modules 
+# importing standard modules 
 import subprocess
 import sys
 import tkinter as tk
 
-#Importin local modules
+# importing local modules
 from color_schemes      import color_scheme
-from Pages.start_page   import StartPage
 
-#Set Color Scheme and Font
+# set color scheme and font
 gui_color=color_scheme(1)                                            # 1=default
 
 class SplashPage(tk.Frame):
@@ -25,17 +24,18 @@ class SplashPage(tk.Frame):
         self.grid_rowconfigure(1,weight=1)
         self.grid_columnconfigure(0,weight=1)
 
-        #background image
-        bg = tk.PhotoImage(file="/pi-wrf/WRF_System/lib/logo_splash_image.gif")
+        # background image
+        bg = tk.PhotoImage(file='/pi-wrf/WRF_System/lib/logo_splash_image.gif')
         logo_bg = tk.Label(self,image=bg) 
         logo_bg.image = bg
         logo_bg.place(x=0,y=0,relwidth=1,relheight=1)
 
-        #continue button
+        # continue button
+        from Pages.start_page   import StartPage
         continue_btn = tk.Button(self,
-                          text="Continue",
-                          font=("Arial Bold",40),
-                          borderwidth=5,bg="white",
+                          text='Continue',
+                          font=('Arial Bold',40),
+                          borderwidth=5,bg='white',
                           activebackground=gui_color[2],
                           width=10,
                           command=lambda : controller.show_frame(StartPage))
--- src/pi-wrf/Pages/start_page.py
+++ src/pi-wrf/Pages/start_page.py
@@ -1,12 +1,12 @@
-#Importing modules 
-import sys
-from importlist import *
+# importing standard modules 
 import requests
-from tkinter import messagebox
+import subprocess
+import sys
+import tkinter as tk
 
-#Set Color Scheme and Font
+# set color scheme and font
+from color_schemes     import color_scheme
 gui_color=color_scheme(1)                                            # 1=default
-LARGE_FONT = ("Verdana", 12)
 
 def check_internet():
     url='http://www.google.com/'
@@ -23,62 +23,74 @@ class StartPage(tk.Frame):
         self.configure(bg=gui_color[0])
         screenwidth=self.winfo_screenwidth()                         #get the current screen width
         screenheight=self.winfo_screenheight()                       #current height of screen
-        subprocess.call('convert /pi-wrf/WRF_System/lib/start_page_bg.jpg -resize {}x{}\! /pi-wrf/WRF_System/lib/start_page_bg.gif'\
+        subprocess.call('convert /pi-wrf/WRF_System/lib/start_page_bg.jpg -resize {}x{}\! '
+                        ' /pi-wrf/WRF_System/lib/start_page_bg.gif'\
                         .format(screenwidth,screenheight),
                         shell=True)
-        photo=tk.PhotoImage(file="/pi-wrf/WRF_System/lib/start_page_bg.gif")
+        photo=tk.PhotoImage(file='/pi-wrf/WRF_System/lib/start_page_bg.gif')
+
+        # configuring layout of widgets
         self.grid_columnconfigure(0,weight=4)
         self.grid_rowconfigure(0,weight=1)
         self.grid_rowconfigure(1,weight=3)
         self.grid_rowconfigure(2,weight=1)
         self.grid_rowconfigure(3,weight=3)
         self.grid_rowconfigure(4,weight=1)
-        bg_label_test=tk.Label(self,image=photo)
-        bg_label_test.image=photo
-        bg_label_test.place(x=0,y=0,relwidth=1,relheight=1)
-
-        topbanner = tk.Label(self,
-                             bg=gui_color[1],
-                             font=("Arial Bold",40),
-                             text="Welcome to the Raspberry Pi-WRF Application")
-        topbanner.grid(column=0,columnspan=3,sticky='new')
         
-        from Pages.page_one   import PageOne
-        btn_1 = tk.Button(self,
-                          text="Run Forecast",
-                          font=("Arial Bold",40),
-                          borderwidth=5,bg=gui_color[2],
-                          activebackground=gui_color[3],
-                          width=20,
-                          command=lambda : [controller.show_frame(PageOne),check_internet()])
-        btn_1.grid(row=1,sticky='s') 
+        # background label
+        bg_lbl = tk.Label(self,image=photo)
+        bg_lbl.image = photo
+        bg_lbl.place(x=0,y=0,relwidth=1,relheight=1)
+
+        # header label
+        header_lbl = tk.Label(self,
+                              bg=gui_color[1],
+                              font=('Arial Bold',40),
+                              text='Welcome to the Raspberry Pi-WRF Application')
+        header_lbl.grid(column=0,columnspan=3,sticky='new')
+        
+        # run forecast button
+        from Pages.page_one  import PageOne #located here to prevent circular imports
+        run_fcst_btn = tk.Button(self,
+                                 text='Run Forecast',
+                                 font=('Arial Bold',40),
+                                 borderwidth=5,bg=gui_color[2],
+                                 activebackground=gui_color[3],
+                                 width=20,
+                                 command=lambda : [controller.show_frame(PageOne),check_internet()])
+        run_fcst_btn.grid(row=1,sticky='s') 
         if check_internet():
-            btn_1.config(command=lambda : [controller.show_frame(PageOne)])
+            run_fcst_btn.config(command=lambda : [controller.show_frame(PageOne)])
         else:
-            btn_1.config(command=lambda : [messagebox.showwarning("Warning", "No network connection detected. cannot run live simulation. Please exit application and check connection.")])                      
-        btn_2 = tk.Button(self,
-                          text="Run Archived Simulation",
-                          font=("Arial Bold",40),
+            run_fcst_btn.config(command=lambda : [tk.messagebox.showwarning('Warning', 'No network connection detected. ' 
+                                                 'cannot run live simulation. Please exit application and check connection.')])
+        # archived sim button
+        arc_sim_btn = tk.Button(self,
+                          text='Run Archived Simulation',
+                          font=('Arial Bold',40),
                           borderwidth=5,
                           bg=gui_color[2],
                           activebackground=gui_color[3],
                           width=20,
-                          command=lambda : messagebox.showwarning("Warning", 
-                                                                   "Archived Simulations Are Not Yet Available"))
-        btn_2.grid(row=2)
-        btn_3 = tk.Button(self,
-                          text="Exit",
-                          font=("Arial Bold",40),
+                          command=lambda : tk.messagebox.showwarning('Warning', 
+                                                                  'Archived Simulations Are Not Yet Available'))
+        arc_sim_btn.grid(row=2)
+        
+        # exit button
+        exit_btn = tk.Button(self,
+                          text='Exit',
+                          font=('Arial Bold',40),
                           borderwidth=5,bg=gui_color[2],
                           activebackground=gui_color[3],
                           width=20,
                           command=lambda : controller.quit_app())
-        btn_3.grid(row=3,sticky='n')
+        exit_btn.grid(row=3,sticky='n')
 
-        version_lable = tk.Label(self,
-                                 text=("Pi-WRF Version 1.2.0"),
-                                 anchor="w",
-                                 font=("Arial Bold",5),
-                                 fg="black",
+        # version label
+        version_lbl = tk.Label(self,
+                                 text=('Pi-WRF Version 1.2.0'),
+                                 anchor='w',
+                                 font=('Arial Bold',5),
+                                 fg='black',
                                  bg=gui_color[1])
-        version_lable.grid(row=4,sticky='sew')
+        version_lbl.grid(row=4,sticky='sew')
