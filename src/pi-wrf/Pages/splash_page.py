5a85bc0c0a612b46c7a01a2b1e32de433fd55b33
wefoust <wefoust@ucar.edu>
2020-11-30 15:51:59 -0500

    Splash page update and change image display

--- src/pi-wrf/Pages/splash_page.py
+++ src/pi-wrf/Pages/splash_page.py
@@ -2,6 +2,7 @@
 import subprocess
 import sys
 import tkinter as tk
+from PIL import Image, ImageTk
 
 # importing local modules
 from color_schemes      import color_scheme
@@ -13,30 +14,38 @@ class SplashPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.configure(bg=gui_color[0])
-        screenwidth=self.winfo_screenwidth()                         # get the current screen width
-        screenheight=self.winfo_screenheight()                       # current height of screen  
-        subprocess.call('convert /pi-wrf/WRF_System/lib/logo_splash_image.jpg -resize {}x{}\! ' 
-                        '/pi-wrf/WRF_System/lib/logo_splash_image.gif'\
-                        .format(screenwidth,screenheight),
-                        shell=True)
 
-        self.grid_rowconfigure(0,weight=1)
-        self.grid_rowconfigure(1,weight=1)
-        self.grid_columnconfigure(0,weight=1)
+        frame = tk.Frame(self, relief='raised', borderwidth=2)
+        frame.pack(fill="both", expand=True)
+        frame.pack_propagate(False)
 
-        # background image
-        bg = tk.PhotoImage(file='/pi-wrf/WRF_System/lib/logo_splash_image.gif')
-        logo_bg = tk.Label(self,image=bg) 
-        logo_bg.image = bg
-        logo_bg.place(x=0,y=0,relwidth=1,relheight=1)
+
+        self.image = Image.open("/pi-wrf/WRF_System/lib/logo_splash_image.jpg")
+        self.img_copy= self.image.copy()
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background = tk.Label(frame, image=self.background_image)
+        self.background.place(x=0, y=0, relwidth=1, relheight=1)
+        self.background.bind('<Configure>', self._resize_image)
+
+
+        top_frame = tk.Frame(frame)
+        top_frame.place(relx=0.5,rely=0.8,anchor='center')
 
         # continue button
         from Pages.start_page   import StartPage
-        continue_btn = tk.Button(self,
+        continue_btn = tk.Button(top_frame,
                           text='Continue',
                           font=('Arial Bold',40),
                           borderwidth=5,bg='white',
                           activebackground=gui_color[2],
                           width=10,
                           command=lambda : controller.show_frame(StartPage))
-        continue_btn.grid(row=1, column=0)
+        continue_btn.pack()
+
+
+    def _resize_image(self,event):
+        new_width = event.width
+        new_height = event.height
+        self.image = self.img_copy.resize((new_width, new_height))
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background.configure(image =  self.background_image)
