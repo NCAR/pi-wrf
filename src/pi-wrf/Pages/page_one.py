07c11bd0be078e7cc516a0fa32652b709f7ecd3e
wefoust <wefoust@ucar.edu>
2020-11-30 16:05:29 -0500

    update listboxes

--- src/pi-wrf/Pages/page_one.py
+++ src/pi-wrf/Pages/page_one.py
@@ -15,7 +15,8 @@ current_time=datetime.datetime.utcnow().replace(microsecond=0,
 
 # rounding current hour to appropriate 6th hour for GFS initital conditions
 if current_time.hour >= 0 and current_time.hour < 3:
-    current_time=current_time-datetime.timedelta(days=1).replace(hour=18)
+    current_time=current_time-datetime.timedelta(days=1)
+    current_time=current_time.replace(hour=18)
 elif current_time.hour >= 3 and current_time.hour < 9:
     current_time=current_time.replace(hour=0)
 elif current_time.hour >= 9 and current_time.hour < 15:
@@ -25,25 +26,24 @@ elif current_time.hour >=15 and current_time.hour < 21:
 elif current_time.hour >=21:
     current_time=current_time.replace(hour=18)
 
-# range and interval of start dates and end dates
-time_interval=6
-days_back=2
-days_ahead=3
+# parameters that alter user's simulation options
+time_interval=6 # hour interval of WRF input files
+days_back=2     # days in past where user can initialize model
+days_ahead=6    # days the user can simulate ahead
+time_delta=datetime.timedelta(hours=time_interval)
 
-init_times=[0]*int(((24/6)*days_back+1))
-end_times=[0]*int(((24/6)*days_ahead+1))
 
-init_time_delta=datetime.timedelta(hours=-time_interval)
-end_time_delta=datetime.timedelta(hours=time_interval)
+#Creating list of initialization times from (days_back to current_time)
+#Lists of simulation end times from (days_back+time_interval to current_time+days_ahead)
+init_times=[]
+init_times.append(current_time)
+for i in range(1,int(24/time_interval*days_back+1),1):
+    init_times.append(init_times[i-1]-time_delta)
 
-init_times[0]=current_time
-for i in range(1,len(init_times),1):
-    init_times[i]=init_times[i-1]+init_time_delta
-
-time_delta=datetime.timedelta(hours=6)
-end_times[0]=current_time
-for i in range(1,len(end_times),1):
-    end_times[i]=end_times[i-1]+end_time_delta
+end_times=[]
+end_times.append(init_times[-2])
+for i in range(1,int((24/time_interval)*(days_ahead+days_back)),1):
+    end_times.append(end_times[i-1]+time_delta)
 
 
 class PageOne(tk.Frame):
@@ -78,6 +78,7 @@ class PageOne(tk.Frame):
             user_duration=datetime.datetime.strptime(user_end_selection,'%m-%d-%Y-%H') - \
                           datetime.datetime.strptime(user_start_selection,'%m-%d-%Y-%H')
             
+            total_hrs = user_duration.total_seconds()/60/60
             if user_duration.total_seconds()/60/60 == 0.0:
                 output=('Start and end time match. Please change times to continue.')
                 domain_btn.config(state='disabled')
@@ -85,8 +86,18 @@ class PageOne(tk.Frame):
                 output=('The duration of your simulation is '+str(user_duration.days)+
                         ' days and '+str(((user_duration.total_seconds()/60)/60)%24)+
                         ' hours.\n Click "Choose Domain" to continue or'+
-                        ' "Reset Selection" to choose new times.') 
+                        ' "Reset Selection" to choose new times.')
+                        
+           
+            
             return output
+ 
+        def populate_end_lbx(event):
+            duration_times_lbx.delete(0,tk.END)
+            usr_sel=start_times_lbx.curselection()
+            for i in range(start_times_lbx.size()-int(usr_sel[0]+1),
+                           start_times_lbx.size()-int(usr_sel[0])+int(24/time_interval*days_ahead)-1,1):
+                duration_times_lbx.insert(i,end_times[i])
 
         def clear_time_menu():
             """ A method that resets the buttons to
@@ -98,7 +109,7 @@ class PageOne(tk.Frame):
             start_times_lbx.config(state='normal')
             duration_times_lbx.config(state='normal')
             start_times_lbx.selection_clear(0,tk.END)
-            duration_times_lbx.selection_clear(0,tk.END)
+            duration_times_lbx.delete(0,tk.END)
  
         # laying out frames for top_banner, listboxes, and buttons
         self.grid_rowconfigure(0,weight=1)
@@ -108,9 +119,11 @@ class PageOne(tk.Frame):
         self.grid_rowconfigure(4,weight=100)
         self.grid_rowconfigure(5,weight=0)
         self.grid_rowconfigure(6,weight=100)
-        self.grid_columnconfigure(0,weight=4)
-        self.grid_columnconfigure(1,weight=4)
-        self.grid_columnconfigure(2,weight=2)
+        self.grid_columnconfigure(0,weight=8)
+        self.grid_columnconfigure(1,weight=8)
+        self.grid_columnconfigure(2,weight=16)
+        self.grid_columnconfigure(3,weight=4)
+
 
         # banners, listboxes, labels, buttons
         top_banner_lbl = tk.Label(self,
@@ -121,7 +134,7 @@ class PageOne(tk.Frame):
         top_banner_lbl.grid(column=0,
                             row=0,
                             sticky='ewns',
-                            columnspan=3)
+                            columnspan=4)
 
         init_time_lbl = tk.Label(self,
                                  text='Start Time',
@@ -138,13 +151,14 @@ class PageOne(tk.Frame):
                                      highlightcolor=gui_color[3],
                                      selectbackground=gui_color[4],
                                      exportselection=0)
+        
         start_times_lbx.grid(column=0,
                              row=2,
                              rowspan=3,
                              sticky='ns')
         for i in range(0,len(init_times),1):
             start_times_lbx.insert(i,init_times[i])
-        
+        start_times_lbx.bind('<<ListboxSelect>>',populate_end_lbx)
         utc_indicator_lbl = tk.Label(self,
                                      text='Times are in UTC',
                                      font=('Arial Bold',10),
@@ -172,9 +186,17 @@ class PageOne(tk.Frame):
         duration_times_lbx.grid(column=1,
                                 row=2,
                                 rowspan=3,
-                                sticky='ns')
-        for i in range(0,len(end_times),1):
-            duration_times_lbx.insert(i,end_times[i])
+                                sticky='nse')
+        #for i in range(0,len(end_times),1):
+        #    duration_times_lbx.insert(i,end_times[i])
+
+        scrollbar = tk.Scrollbar(self)
+        scrollbar.grid(column=2,
+                       row=2,
+                       rowspan=3,
+                       sticky='nsw')
+        duration_times_lbx.config(yscrollcommand = scrollbar.set)
+        scrollbar.config(command=duration_times_lbx.yview)
 
         utc_indicator_2_lbl = tk.Label(self,
                                  text='Times are in UTC',
@@ -187,8 +209,8 @@ class PageOne(tk.Frame):
         
         buttons_frame=tk.Frame(self,bg=gui_color[0])
         buttons_frame.grid(row=3,
-                           column=2,
-                           sticky='ewns')
+                           column=3,
+                           sticky='wns')
   
         from Pages.start_page  import StartPage
         home_btn = tk.Button(buttons_frame,
@@ -242,6 +264,6 @@ class PageOne(tk.Frame):
         duration_text_lbl.grid(column=0,
                                row=6,
                                sticky='ewns',
-                               columnspan=2)
+                               columnspan=3)
 
 
